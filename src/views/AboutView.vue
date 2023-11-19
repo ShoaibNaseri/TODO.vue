755650f19200fff0b4023157a73020e4b82952cc
ShoaibNaseri <shoaib.hasib10@gamil.com>
2023-11-19 22:55:11 +0430

    dd

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>Not about this this is commit  dd update about sdfsdf sdfsdlf is vue Page</h1>
+    <h1>Not about this this is commit update dd update about sdfsdf sdfsdlf is vue Page</h1>
   </div>
 </template>
 
